98f36e56d8fe5e73146974124e9352c642356ce5
SpyderrHub <abhishekmundu@myyahoo.com>
2025-06-11 22:43:56 +0530

    Update configure.sh

--- mobile/android/branding/official/configure.sh
+++ mobile/android/branding/official/configure.sh
@@ -2,5 +2,5 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-MOZ_APP_DISPLAYNAME=Or Browser
+MOZ_APP_DISPLAYNAME="Or Browser"
 ANDROID_PACKAGE_NAME="org.android.or.browser"
