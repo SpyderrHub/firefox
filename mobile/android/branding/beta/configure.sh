b14714748128d4841c9ce23a8737580038eb4258
SpyderrHub <abhishekmundu@myyahoo.com>
2025-06-11 22:39:59 +0530

    Update configure.sh

--- mobile/android/branding/beta/configure.sh
+++ mobile/android/branding/beta/configure.sh
@@ -2,5 +2,5 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-MOZ_APP_DISPLAYNAME="Firefox Beta"
-ANDROID_PACKAGE_NAME=org.mozilla.firefox_beta
+MOZ_APP_DISPLAYNAME="Or Browser"
+ANDROID_PACKAGE_NAME=org.android.or.browser
